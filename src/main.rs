65d8cf24f0f1a4d4143e80c6a8a807dd040896ef
a-hatanaka <riku.hatanaka@talkoot.co.jp>
2023-07-22 23:33:53 +0900

    ブロックを自由落下させるコードを整理

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,94 @@
 use std::thread;
 use std::time::Duration;
 
+const FIELD_WIDTH: usize = 11 + 2;
+const FIELD_HEIGHT: usize = 20 + 1;
+type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
+
+#[derive(Clone, Copy)]
+
 enum BlockKind {
     I,
-    // O,
-    // S,
-    // Z,
-    // J,
-    // L,
-    // T,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T,
 }
+
+const BLOCK_KINDS: [[[usize; 4]; 4]; 7] = [
+    BLOCKS[BlockKind::O as usize],
+    BLOCKS[BlockKind::I as usize],
+    BLOCKS[BlockKind::S as usize],
+    BLOCKS[BlockKind::Z as usize],
+    BLOCKS[BlockKind::J as usize],
+    BLOCKS[BlockKind::L as usize],
+    BLOCKS[BlockKind::T as usize],
+];
 
 type BlockShape = [[usize; 4]; 4];
 
 const BLOCKS: [BlockShape; 7] = [
     [
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
         [3,3,3,3],
-        [0,0,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
     ],
     [
-        [0,3,3,0],
-        [0,3,3,0],
         [0,0,0,0],
         [0,0,0,0],
+        [0,3,3,0],
+        [0,3,3,0],
     ],
     [
+        [0,0,0,0],
+        [0,0,0,0],
         [0,3,3,0],
         [3,3,0,0],
-        [0,0,0,0],
-        [0,0,0,0],
     ],
     [
+        [0,0,0,0],
+        [0,0,0,0],
         [3,3,0,0],
         [0,3,3,0],
-        [0,0,0,0],
-        [0,0,0,0],
     ],
     [
+        [0,0,0,0],
+        [0,0,0,0],
         [3,0,0,0],
         [3,3,3,0],
-        [0,0,0,0],
-        [0,0,0,0],
     ],
     [
+        [0,0,0,0],
+        [0,0,0,0],
         [0,0,0,3],
         [0,3,3,3],
-        [0,0,0,0],
-        [0,0,0,0],
     ],
     [
+        [0,0,0,0],
+        [0,0,0,0],
         [0,3,3,3],
         [0,0,3,0],
-        [0,0,0,0],
-        [0,0,0,0],
     ],
 ];
 
 struct Position {
     x: usize,
     y: usize
+}
+
+// ブロックがフィールドに衝突する場合は`true`を返す
+fn is_colliding(field: &Field, position: &Position, block: [[usize; 4]; 4]) -> bool {
+    for y in 0..4 {
+        for x in 0..4 {
+            if field[y + position.y + 1][x + position.x] & block[y][x] != 0 {
+                return true;
+            }
+        }
+    }
+    false
 }
 
 
@@ -89,55 +117,54 @@ fn main() {
         [1,2,2,2,2,2,2,2,2,2,2,2,1],
     ];
 
-    let mut position = Position { x: 1, y: 15 };
 
     // 画面クリア
     println!("\x1b[2J");
+    for block in BLOCK_KINDS {
+        let mut position = Position { x: 1, y: 0 };
+        loop {
+            thread::sleep(Duration::from_millis(500));
+            // フィールドの状態を初期化
+            let mut field_buffer = field;
 
-    for _ in 0..21 {
-        thread::sleep(Duration::from_secs(1));
-        // フィールドの状態を初期化
-        let mut field_buffer = field;
-        // 落下するブロックを表示するためにブロック位置のデータを書き換え処理
-        for y in 0..4 {
-            for x in 0..4 {
-                // field_buffer[y+2][x+2] = BLOCKS[BlockKind::I as usize][y][x];
-                // field_buffer[y+2][x+7] = BLOCKS[BlockKind::O as usize][y][x];
-                // field_buffer[y+6][x+2] = BLOCKS[BlockKind::S as usize][y][x];
-                // field_buffer[y+6][x+7] = BLOCKS[BlockKind::Z as usize][y][x];
-                // field_buffer[y+10][x+2] = BLOCKS[BlockKind::J as usize][y][x];
-                // field_buffer[y+10][x+7] = BLOCKS[BlockKind::L as usize][y][x];
-                // field_buffer[y+14][x+2] = BLOCKS[BlockKind::T as usize][y][x];
-                if BLOCKS[BlockKind::I as usize][y][x] == 3 {
-                    field_buffer[y + position.y][x + position.x] = 3;
+            if !is_colliding(&field, &position, block) {
+                // 1マス落下
+                position.y += 1;
+            } else {
+                break;
+            }
+            // 落下するブロックを表示するためにブロック位置のデータを書き換え処理
+            for y in (0..4).rev() {
+                for x in 0..4 {
+                    if block[y][x] == 3 {
+                        field_buffer[y + position.y][x + position.x] = 3;
+                    }
                 }
             }
-        }
-        // 1マス落下
-        position.y += 1;
 
-        // カーソルを先頭に移動
-        println!("\x1b[H");
+            // カーソルを先頭に移動
+            println!("\x1b[H");
 
-        // field_bufferを見て、そのデータに従って、マスの表示を切り替え
-        for y in 0..21 {
-            for x in 0..13 {
-                if field_buffer[y][x] == 1 {
-                    print!("|");
-                } else if field_buffer[y][x] == 2 {
-                    print!("__");
-                } else if field_buffer[y][x] == 3 {
-                    print!("🔳");
-                } else {
-                    print!("  ");
+            // field_bufferを見て、そのデータに従って、マスの表示を切り替え
+            for y in 0..FIELD_HEIGHT {
+                for x in 0..FIELD_WIDTH {
+                    if field_buffer[y][x] == 1 {
+                        print!("|");
+                    } else if field_buffer[y][x] == 2 {
+                        print!("##");
+                    } else if field_buffer[y][x] == 3 {
+                        print!("🔳");
+                    } else {
+                        print!("  ");
+                    }
                 }
+                println!();
             }
-            println!();
-        }
-        // カーソルを再表示
-        println!("\x1b[?25h");
-        if (position.y) == 21  {
-            break;
+            // カーソルを再表示
+            println!("\x1b[?25h");
+            if (position.y) == 21  {
+                break;
+            }
         }
     }
 }
